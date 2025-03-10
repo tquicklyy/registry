15a7937f610432e4c1da12949f765739fb26a8a2
Gleb <tquicklyy@yandex.ru>
2025-03-10 20:11:24 +0300

    Initial commit

---
+++ src/main/java/com/registry/office/system/registry_office_system/RegistryOfficeSystemApplication.java
@@ -0,0 +1,13 @@
+package com.registry.office.system.registry_office_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistryOfficeSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistryOfficeSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/registry/office/system/registry_office_system/RegistryOfficeSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.registry.office.system.registry_office_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistryOfficeSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
