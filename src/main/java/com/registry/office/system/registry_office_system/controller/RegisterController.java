4a5a63ff1ce4ad0c77d1a6c7bfdb61bdf88c529d
Gleb <tquicklyy@yandex.ru>
2025-04-03 17:43:11 +0300

    Add error display and update input with type date #6

--- src/main/java/com/registry/office/system/registry_office_system/controller/RegisterController.java
+++ src/main/java/com/registry/office/system/registry_office_system/controller/RegisterController.java
@@ -1,13 +1,24 @@
 package com.registry.office.system.registry_office_system.controller;
 
+import com.registry.office.system.registry_office_system.entity.User;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class RegisterController {
 
     @GetMapping("/register")
-    public String getRegisterForm() {
+    public String getRegisterForm(User user) {
         return "register";
+    }
+
+    @PostMapping("/register")
+    public String confirmRegister(@Valid @ModelAttribute("user") User user, BindingResult result) {
+        if(result.hasErrors()) return "register";
+        return "login";
     }
 }
