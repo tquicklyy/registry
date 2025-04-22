cd97087eac58e4e1c2682b59181f847d779557c4
Gleb <tquicklyy@yandex.ru>
2025-04-22 22:33:11 +0300

    Add admin window #9

--- src/main/java/com/registry/office/system/registry_office_system/config/SecurityConfig.java
+++ src/main/java/com/registry/office/system/registry_office_system/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.RememberMeConfigurer;
@@ -15,7 +16,8 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.rememberme.TokenBasedRememberMeServices;
 
 @Configuration
-@EnableWebSecurity
+@EnableWebSecurity // Проверка безопасности на уровне конфига с фильром HTTP
+@EnableMethodSecurity // Проверка безопасности на уровне методов, то есть разные аннотации теперь учитываются
 public class SecurityConfig {
 
     @Bean
@@ -33,7 +35,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/", "/login", "/register", "/css/**").permitAll()
+                        .requestMatchers("/", "/login", "/register", "/css/**", "/admin/**").permitAll()
                         .requestMatchers("/documents").authenticated()
                         .anyRequest().authenticated()
                 )
