6a59e8f4aca46a1d804cca82b56030a6510f89fc
Gleb <tquicklyy@yandex.ru>
2025-04-26 09:14:27 +0300

    Update security config

--- src/main/java/com/registry/office/system/registry_office_system/config/SecurityConfig.java
+++ src/main/java/com/registry/office/system/registry_office_system/config/SecurityConfig.java
@@ -36,7 +36,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/", "/login", "/register", "/css/**", "/admin/**", "/images/**").permitAll()
+                        .requestMatchers("/", "/login", "/register", "/css/**", "/info", "/admin/**", "/images/**").permitAll()
                         .requestMatchers("/documents").authenticated()
                         .anyRequest().authenticated()
                 )
